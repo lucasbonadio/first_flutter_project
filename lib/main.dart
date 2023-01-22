91bdfb431b5dfe1cd43a27a4be8b676c35aabc04
Lucas Bonadio <lucasbonadio2@hotmail.com>
2023-01-22 13:47:49 -0300

    feature: first commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter'),
+        ),
+        body: ListView(
+          children: [
+            Task('Play Video Game aaaaaaaaaaaaaaaaaa'),
+            Task('Buy Clothes'),
+            Task('Learn Flutter'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String name;
+
+  const Task(this.name, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int level = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Column(
+            children: [
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Container(
+                        width: 200,
+                        child: Text(
+                          widget.name,
+                          style: TextStyle(
+                            fontSize: 24,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        )),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState((){
+                            level++;
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_up)),
+                  ],
+                ),
+              ),
+              Text(
+                'level: $level',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/my_first_screen.dart
@@ -0,0 +1,66 @@
+// Column(
+// crossAxisAlignment: CrossAxisAlignment.center,
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// children: [
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(
+// width: 150,
+// height: 150,
+// color: Colors.red,
+// ),
+// Container(
+// width: 75,
+// height: 75,
+// color: Colors.blue,
+// ),
+// Container(
+// width: 37,
+// height: 37,
+// color: Colors.yellow,
+// ),
+// ],
+// ),
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(
+// width: 150,
+// height: 150,
+// color: Colors.red,
+// ),
+// Container(
+// width: 75,
+// height: 75,
+// color: Colors.blue,
+// ),
+// Container(
+// width: 37,
+// height: 37,
+// color: Colors.yellow,
+// ),
+// ],
+// ),
+// Row(
+// crossAxisAlignment: CrossAxisAlignment.center,
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// children: [
+// Container(
+// width: 50,
+// height: 50,
+// color: Colors.green,
+// ),
+// Container(
+// width: 50,
+// height: 50,
+// color: Colors.pink,
+// ),
+// Container(
+// width: 50,
+// height: 50,
+// color: Colors.deepPurple,
+// )
+// ],
+// )
+// ])
